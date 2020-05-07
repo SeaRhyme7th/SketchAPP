82637621a139fb1b51ddbb600c1e30fe822b8f99
jjkislele <jjkjjk333@sina.com>
2020-05-07 15:57:00 +0800

    chore: add model download link

--- download_prerequisites.sh
+++ download_prerequisites.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
 
+# offline features
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Z1fbJrNnjD7VrubYBCtfweYNoMjH1uEW' -O feat_photo.npz
 mv feat_photo.npz SketchTriplet/out_feat_flickr15k_1904041458/
 
+# image dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PqzIO-OWTeEAl3Hs5tRavRs6-qZ8OmXb' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1PqzIO-OWTeEAl3Hs5tRavRs6-qZ8OmXb" -O resize_img.zip
 rm /tmp/cookies.txt
 unzip resize_img.zip
 mv resize_img/* static/images/dataset/
 rm -r resize_img
+
+# offline model
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oUDCTENBzdBok7rjB_B8zHE3mwdw_0ve' -O 500.pth
+mv 500.pth static/model/
